2b44572b370878c2a5372a5a3959e973d6fa3cbc
lixcel <lixcelv@gmail.com>
2022-03-04 06:15:01 +0000

    [add]devise install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "d97b4dd693664503aded7f826770e601.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
