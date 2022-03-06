1952cded31994a184370cdcbcab927c6fe5b434a
lixcel <lixcelv@gmail.com>
2022-03-06 03:51:11 +0000

    [add]tagモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_051815) do
+ActiveRecord::Schema.define(version: 2022_03_06_034148) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 2022_03_05_051815) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "kind", default: 0
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
