fd9949b67c780c82212f481f33dec0574dc53902
lixcel <lixcelv@gmail.com>
2022-03-19 04:03:35 +0000

    [update]layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @post = @user.posts.all.order(created_at: :desc).page(params[:page]).per(3)
+    @favorite = Favorite.where(user_id: @user.id).order(created_at: :desc).page(params[:page]).per(3)
   end
 
   def index
