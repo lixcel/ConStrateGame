9a0942cdbd1b7c66ccd70f514aba5b99c9c74530
lixcel <lixcelv@gmail.com>
2022-03-21 04:27:51 +0000

    修正撤回

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
-require "uglifier"
+re
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
