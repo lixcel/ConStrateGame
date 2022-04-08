026f40fcc70bf9a1c5d6c537f4bc9c9dd579e558
lixcel <lixcelv@gmail.com>
2022-04-08 01:53:10 +0000

    [update]フォロー・フォロワーページの修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -13,14 +13,14 @@ class RelationshipsController < ApplicationController
 
   # フォロー一覧
   def followings
-    user = User.find(params[:user_id])
-    @user = user.followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings
   end
 
   # フォロワー一覧
   def followers
-    user = User.find(params[:user_id])
-    @user = user.followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers
   end
 
 end
